72cab1b04700b12389e8655b28b96e0fb88a111f
Sebastian Salguero <sebasalguero67@gmail.com>
2022-03-08 02:12:07 -0300

    EJ02 sin iterador

---
+++ EJ02_RazasPerros_con_Iterador/src/ej02_razasperros_con_iterador/EJ02_RazasPerros_con_Iterador.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ej02_razasperros_con_iterador;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EJ02_RazasPerros_con_Iterador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
